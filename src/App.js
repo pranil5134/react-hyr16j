a467c097b2ab50171be752297ad1145d0ab1bb98
pranil5134 <pranilsatane@gmail.com>
2021-05-22 00:27:34 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import axios from 'axios';
 
-export default function App() {
+export default class App extends React.Component {
+  
+  constructor (props){
+    super (props)
+    this.state={
+      posts:[]
+    };
+  }
+
+  //Create
+  //update
+  //read
+  //delete
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
