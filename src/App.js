df3cba19e041b546ce630cf30b9aa6dd8acb0f98
pranil5134 <pranilsatane@gmail.com>
2021-05-22 00:36:55 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,22 +3,36 @@ import './style.css';
 import axios from 'axios';
 
 export default class App extends React.Component {
-  
-  constructor (props){
-    super (props)
-    this.state={
-      posts:[]
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
     };
   }
 
   //Create
+
   //update
   //read
+  getPost = async () => {
+    const { data } = await axios.get(
+      'https://jsonplaceholder.typicode.com/posts'
+    );
+    console.log(data);
+  };
   //delete
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h1>Blog Post</h1>
+        <table>
+          <thead>
+            <th>id</th>
+            <th>titile</th>
+            <th>body</th>
+          </thead>
+        </table>
+      </div>
+    );
+  }
 }
